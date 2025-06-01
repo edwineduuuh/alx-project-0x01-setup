879802ed487a135c6ec927f4063e0d7b456be7fa
unknown <edumwas92@gmail.com>
2025-06-01 14:09:31 +0300

    Extend the posts page

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -15,6 +15,18 @@ export interface UserProps {
     city:    string;
     zipcode: string;
     catchPhrase: string;
+}
+  
+export interface PostData {
+    userId: number;
+    id?: number;
+    title: string;
+    body: string;
+  }
+  
+  export interface PostModalProps {
+    onClose: () => void;
+    onSubmit: (post: PostData) => void;
   }
   
   
